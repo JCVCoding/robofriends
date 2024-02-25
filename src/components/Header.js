470282024686ba26efea752644bcf7e406f06f45
Josh <jvcoding9407@gmail.com>
2024-02-24 22:03:53 -0500

    Add CounterButton component. Place component in Header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
 import React, { memo } from 'react';
+import CounterButton from './CounterButton';
 
 const Header = memo(function Header() {
-  return <h1 className='f1'>RoboFriends</h1>;
+  return (
+    <>
+      <h1 className='f1'>RoboFriends</h1>
+      <CounterButton />
+    </>
+  );
 });
 export default Header;
